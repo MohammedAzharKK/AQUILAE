df93852a8de85b43aad632986917d0a57a9d4f3d
SAURAVKUMARTM <sauravzen60@gmail.com>
2024-07-10 12:44:02 +0530

    pages

--- lib/view/pages/splash_screen.dart
+++ lib/view/pages/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aqualae_apk/view/pages/first_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -5,6 +6,19 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Column(
+      children: [
+        ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const FirstPage(),
+                  ));
+            },
+            child: const Text("Next Page"))
+      ],
+    ));
   }
 }
