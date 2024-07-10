ee1aee1ff005623278a7a701c93c3a8183218753
MohammedAzharKK <azramn6993@gmail.com>
2024-07-10 15:00:29 +0530

    splash screen compleated

--- lib/view/pages/splash_screen.dart
+++ lib/view/pages/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:aqualae_apk/view/pages/first_page.dart';
 import 'package:flutter/material.dart';
+import 'package:tbib_splash_screen/splash_screen_view.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -17,7 +19,6 @@ class SplashScreen extends StatelessWidget {
           ),
         ),
         Container(
-            height: double.infinity,
             width: double.infinity,
             // color: Colors.blue[800],
             color: const Color.fromARGB(255, 3, 50, 106),
@@ -25,14 +26,30 @@ class SplashScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image.asset(
-                  'assets/images/AquilaeAirways.jpg',
+                // //   Image.asset(
+                // //     'assets/images/AquilaeAirways.jpg',
+                // //   ),
+                // //   const SizedBox(
+                // //     height: 48,
+                // //   ),
+                // //   ElevatedButton(
+                // //       onPressed: () {}, child: const Icon(Icons.forward))
+                Expanded(
+                  child: SplashScreenView(
+                    navigateWhere: true,
+                    navigateRoute: const FirstPage(),
+                    text: WavyAnimatedText(
+                      " Aquilae Airways",
+                      textStyle: const TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    imageSrc: 'assets/images/AquilaeAirways.jpg',
+                    logoSize: 300,
+                  ),
                 ),
-                const SizedBox(
-                  height: 48,
-                ),
-                ElevatedButton(
-                    onPressed: () {}, child: const Icon(Icons.forward))
               ],
             )),
       ]),
